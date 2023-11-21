18117fb9574e681fdf416fc0775a0b28a2a54c87
Anna Putod <anna.putod@edu.ece.fr>
2023-11-21 08:43:07 +0100

    fichier txt

---
+++ cmake-build-debug/CMakeFiles/projet_TG1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/projet_TG1.dir/main.c.obj"
+  "CMakeFiles/projet_TG1.dir/main.c.obj.d"
+  "libprojet_TG1.dll.a"
+  "projet_TG1.exe"
+  "projet_TG1.exe.manifest"
+  "projet_TG1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/projet_TG1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
