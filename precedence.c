f93fbb637c536941fd44ab6ef03bcfce19669f08
Alexia <alexia.farigoul@edu.ece.fr>
2023-11-21 11:33:47 +0100

    debut precedence

---
+++ precedence.c
@@ -0,0 +1,3 @@
+#include <stdio.h>
+#include <stdlib.h>
+//Sous programme qui fait la precedence de la chaine
---
+++ precedence.h
@@ -0,0 +1,17 @@
+//
+// Created by Utilisateur on 21/11/2023.
+//
+
+#ifndef PROJET_TG1_PRECEDENCE_H
+#define PROJET_TG1_PRECEDENCE_H
+
+
+typedef struct graphe{
+    int ordre;
+    int* tache;
+
+}t_graphe;
+
+
+
+#endif //PROJET_TG1_PRECEDENCE_H
