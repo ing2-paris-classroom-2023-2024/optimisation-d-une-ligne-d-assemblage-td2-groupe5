2b18f461b0cc55fcea5a54e2d3f0b41b00771dbc
shaïma <114345002+shm0m@users.noreply.github.com>
2023-11-26 17:44:36 +0100

    ajout des commentaires

--- main_exclusions.c
+++ main_exclusions.c
@@ -1,22 +1,15 @@
-#include "source.h"
-#include "source.c"
+#include "source.h" //bibliotheque contenant les prototypes des fonctions et les structures
+#include "source.c" //bibliotheque contenant les fonctions du projet implémentées
 
-int main() {
-    t_graphe *graphe = lire_graphe("exclusions.txt");
-    afficher_graphe(graphe);
+int main() { //fonction principale du programme 
+    t_graphe *graphe = lire_graphe("exclusions.txt"); //lecture du graphe à partir du fichier "exclusions.txt"
+    afficher_graphe(graphe); //affichage du graphe
+    generer_fichier("graphe.txt", graphe); //génération du fichier "graphe.txt" contenant les informations du graphe
+    colorer_graphe(graphe); //coloration du graphe (welsch-powell)
+    afficher_coloration(graphe); //affichage de la coloration du graphe 
+    liberer_graphe(graphe); //libération de la mémoire allouée pour le graphe
+    return 0; //fin du programme
+} //fin de la fonction principale
 
-    // Génération du fichier avec les informations de sommets et d'arêtes
-    generer_fichier("graphe.txt", graphe);
 
-    // Coloration du graphe
-    colorer_graphe(graphe);
-
-    // Affichage de la coloration
-    afficher_coloration(graphe);
-
-    // Libération des ressources
-    liberer_graphe(graphe);
-
-    return 0;
-}
 
